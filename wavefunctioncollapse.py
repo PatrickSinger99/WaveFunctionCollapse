00578a8157676439b4962a2df61b0784cbb4bc28
Patrick Singer <60189276+PatrickSinger99@users.noreply.github.com>
2023-02-20 12:36:06 +0100

    Update wavefunctioncollapse.py

--- wavefunctioncollapse.py
+++ wavefunctioncollapse.py
@@ -7,18 +7,35 @@ class WaveFunctionCollapse:
         self._grid_size = grid_size
 
         self._grid = [[0]*grid_size for _ in range(grid_size)]
-        self._neighboring_tiles = []
+        self._next_gen_rel_tiles = []
 
     def first_gen(self):
         start_pos = (random.randint(0, self._grid_size - 1), random.randint(0, self._grid_size - 1))
         self._grid[start_pos[0]][start_pos[1]] = random.randint(1, self._tile_amount)
-        self._neighboring_tiles = self.get_neighbors(start_pos[0], start_pos[1])
+        self._next_gen_rel_tiles = self.get_neighbors(start_pos[0], start_pos[1])
 
     def next_gen(self):
-        for tile in self._neighboring_tiles:
-            tile_neighbors = self.get_neighbors(tile[0], tile[1])
-            
-            self._grid[tile[0]][tile[1]] = 6
+        new_next_gen_rel_tiles = []
+        for tile in self._next_gen_rel_tiles:
+            new_next_gen_rel_tiles += self.get_neighbors(tile[0], tile[1])
+            possible_values = self.get_possible_values_based_on_neighbors(tile[0], tile[1])
+
+            if self._grid[tile[0]][tile[1]] == 0:
+                self._grid[tile[0]][tile[1]] = random.choice(possible_values)
+
+        self._next_gen_rel_tiles = new_next_gen_rel_tiles
+
+    def run_simulation(self, show_prints=False):
+        self.first_gen()
+        last_gen_grid = None
+        while last_gen_grid != self._grid:
+            self.next_gen()
+
+            if show_prints:
+                print(self)
+
+            last_gen_grid = 
+
 
     def get_neighbors(self, x, y):
         return_list = []
@@ -29,6 +46,22 @@ class WaveFunctionCollapse:
                 return_list.append((x+check[0], y+check[1]))
 
         return return_list
+
+    def get_possible_values_based_on_neighbors(self, x, y):
+        possibility_space = [i+1 for i in range(self._tile_amount)]
+        neighbors = self.get_neighbors(x, y)
+
+        for neighbor_x, neighbor_y in neighbors:
+            if self._grid[neighbor_x][neighbor_y] != 0:
+
+                new_possibility_space = []
+                for value in possibility_space:
+                    if self._grid[neighbor_x][neighbor_y]-1 <= value <= self._grid[neighbor_x][neighbor_y]+1:
+                        new_possibility_space.append(value)
+
+                possibility_space = new_possibility_space
+
+        return possibility_space
 
     def __str__(self):
         return_string = ""
@@ -42,5 +75,9 @@ if __name__ == "__main__":
 
     wfc = WaveFunctionCollapse(4, 5)
     wfc.first_gen()
+    wfc.next_gen()
+    print(wfc)
+    wfc.next_gen()
+    print(wfc)
     wfc.next_gen()
     print(wfc)
